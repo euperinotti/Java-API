3400c0241dff9a21d2f384934a844f219ee0d102
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-04-25 10:10:45 -0300

    refactor rotas

--- src/backend/projeto/src/main/java/br/com/api/controller/UserController.java
+++ src/backend/projeto/src/main/java/br/com/api/controller/UserController.java
@@ -4,19 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.DAO.IUser;
 import br.com.api.model.User;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UserController {
 
   @Autowired
   private IUser dao;
 
-  @GetMapping("/usuarios")
+  @GetMapping
   public List<User> getAllUsers() {
     return (List<User>) dao.findAll();
+  }
+
+  @PostMapping
+  public User newUser(@RequestBody User usuario){
+    User newUser = dao.save(usuario);
+    return newUser;
   }
 }
