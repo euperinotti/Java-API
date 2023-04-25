1ea89f6d37eefa56e38a13c97a8c49bc7709b21c
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-04-25 10:42:02 -0300

    rota put

--- src/backend/projeto/src/main/java/br/com/api/controller/UserController.java
+++ src/backend/projeto/src/main/java/br/com/api/controller/UserController.java
@@ -1,10 +1,13 @@
 package br.com.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,17 @@ public class UserController {
   public User newUser(@RequestBody User usuario){
     User newUser = dao.save(usuario);
     return newUser;
+  }
+
+  @PutMapping("/{id}")
+  public User editUser(@PathVariable Integer id, @RequestBody User usuario){
+    Optional<User> user = dao.findById(id);
+    User editedUser = user.get();
+
+    if(editedUser != null) {
+      editedUser = dao.save(usuario);
+    }
+
+    return editedUser;
   }
 }
